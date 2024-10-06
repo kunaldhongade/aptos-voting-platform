268570b98ab0ef50a84ef452ab5c2bfe85163f72
kunaldhongade <kunaldhongade1@gmail.com>
2024-10-07 00:13:09 +0530

    Update: Smart contract deployed to testnet

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,5 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
-export const MODULE_ADDRESS = "0xab53787e46dd43fbfe3f36d6cf4c6f9fbcaca563be7e0da5dc89de4891203915";
+export const MODULE_ADDRESS = "0x635bdbc07d1c2e273d1f2cf27e8c87103928f03a7d4ce1760bd9baa6b784eadc";
 export const CREATOR_ADDRESS = import.meta.env.VITE_COLLECTION_CREATOR_ADDRESS;
 export const COLLECTION_ADDRESS = import.meta.env.VITE_COLLECTION_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
